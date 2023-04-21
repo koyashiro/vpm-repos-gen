31aedf44d178e0406894f84339e278b7df3ba876
koyashiro <6698252+koyashiro@users.noreply.github.com>
2023-04-22 04:16:01 +0900

    feat: support for parsing environment variables

--- src/args.rs
+++ src/args.rs
@@ -7,21 +7,21 @@ pub const STDIN: &str = "-";
 #[derive(Debug, Parser)]
 #[command(author, version, about, long_about = None)]
 pub struct Args {
-    #[arg(long, default_value = "")]
+    #[arg(long, env, default_value = "")]
     pub name: String,
 
-    #[arg(long, default_value = "")]
+    #[arg(long, env, default_value = "")]
     pub author: String,
 
-    #[arg(long, default_value = "")]
+    #[arg(long, env, default_value = "")]
     pub url: String,
 
-    #[arg(long, default_value = "")]
+    #[arg(long, env, default_value = "")]
     pub id: String,
 
-    #[arg(long, value_delimiter = ',')]
+    #[arg(long, env, value_delimiter = ',')]
     pub repos: Vec<GitHubRepo>,
 
-    #[arg(long, short, default_value = STDIN)]
+    #[arg(long, short, env, default_value = STDIN)]
     pub output: String,
 }
