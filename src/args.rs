8d045045013193f6a225b8b3717da07ab0c0f243
koyashiro <6698252+koyashiro@users.noreply.github.com>
2023-04-22 02:55:54 +0900

    feat: allow empty input in `--repos` option

--- src/args.rs
+++ src/args.rs
@@ -1,6 +1,8 @@
+use std::str::FromStr;
+
 use clap::Parser;
 
-use crate::github_repo::GitHubRepo;
+use crate::github_repo::{GitHubRepo, ParseError};
 
 #[derive(Debug, Parser)]
 #[command(author, version, about, long_about = None)]
@@ -17,6 +19,51 @@ pub struct Args {
     #[arg(long, default_value = "")]
     pub id: String,
 
-    #[arg(long, value_delimiter = ',')]
+    #[arg(long, value_parser = parse_repos)]
     pub repos: Vec<GitHubRepo>,
+}
+
+fn parse_repos(arg: &str) -> Result<Vec<GitHubRepo>, ParseError> {
+    let args: Vec<&str> = arg.split(',').map(|s| s.trim()).collect();
+    if args.len() == 1 && args[0].is_empty() {
+        return Ok(Default::default());
+    }
+    args.into_iter().map(FromStr::from_str).collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parse_repos_test() {
+        assert_eq!(parse_repos(""), Ok(vec![]));
+        assert_eq!(
+            parse_repos("octocat/linguist"),
+            Ok(vec![GitHubRepo {
+                owner: "octocat".to_string(),
+                repo: "linguist".to_string(),
+            }])
+        );
+        assert_eq!(
+            parse_repos("octocat/linguist"),
+            Ok(vec![GitHubRepo {
+                owner: "octocat".to_string(),
+                repo: "linguist".to_string(),
+            }])
+        );
+        assert_eq!(
+            parse_repos("octocat/linguist,octocat/hello-world"),
+            Ok(vec![
+                GitHubRepo {
+                    owner: "octocat".to_string(),
+                    repo: "linguist".to_string(),
+                },
+                GitHubRepo {
+                    owner: "octocat".to_string(),
+                    repo: "hello-world".to_string(),
+                },
+            ])
+        );
+    }
 }
