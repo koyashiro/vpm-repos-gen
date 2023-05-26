261be9a7dc48d370b29044b84c6330b192efd2f9
koyashiro <6698252+koyashiro@users.noreply.github.com>
2023-05-27 00:54:47 +0900

    feat: add `--pretty` and `--no-pretty` option

--- src/args.rs
+++ src/args.rs
@@ -1,6 +1,11 @@
+use std::{
+    fs::File,
+    io::{self, Write},
+};
+
 use clap::Parser;
 
-use crate::github_repo::GitHubRepo;
+use crate::{github_repo::GitHubRepo, vpm::VpmRepos};
 
 pub const STDIN: &str = "-";
 
@@ -24,4 +29,28 @@ pub struct Args {
 
     #[arg(long, short, env, default_value = STDIN)]
     pub output: String,
+
+    #[arg(long, env, overrides_with = "no_pretty")]
+    pub pretty: bool,
+
+    #[arg(long, env, overrides_with = "pretty")]
+    pub no_pretty: bool,
+}
+
+impl Args {
+    pub fn writer(&self) -> Result<Box<dyn Write>, io::Error> {
+        if self.output.as_str() == STDIN {
+            Ok(Box::new(io::stdout()))
+        } else {
+            Ok(Box::new(File::create(&self.output)?))
+        }
+    }
+
+    pub fn write_json_fn(&self) -> fn(Box<dyn Write>, &VpmRepos) -> serde_json::Result<()> {
+        if self.no_pretty {
+            serde_json::to_writer
+        } else {
+            serde_json::to_writer_pretty
+        }
+    }
 }
